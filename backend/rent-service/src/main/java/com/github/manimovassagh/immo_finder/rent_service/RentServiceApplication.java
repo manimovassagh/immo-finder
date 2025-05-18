cb25c9f6041bc4189e9996c5fa358cb02035aef5
Mani Movassagh <mani.mgh@gmail.com>
2025-05-18 15:27:36 +0200

    Fix RentController class structure and add Spring DevTools for live reload

--- backend/rent-service/src/main/java/com/github/manimovassagh/immo_finder/rent_service/RentServiceApplication.java
+++ backend/rent-service/src/main/java/com/github/manimovassagh/immo_finder/rent_service/RentServiceApplication.java
@@ -2,6 +2,9 @@ package com.github.manimovassagh.immo_finder.rent_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class RentServiceApplication {
@@ -9,5 +12,13 @@ public class RentServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RentServiceApplication.class, args);
 	}
+}
 
+@RestController
+@RequestMapping("/api/v1/rent")
+class RentController {
+	@GetMapping
+	public String getRent() {
+		return "Hello World";
+	}
 }
