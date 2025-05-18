ecfc089a2019e234ed01942ed3abd1a5b2e9b413
Mani Movassagh <mani.mgh@gmail.com>
2025-05-18 15:28:24 +0200

    Clean up RentServiceApplication class

--- backend/rent-service/src/main/java/com/github/manimovassagh/immo_finder/rent_service/RentServiceApplication.java
+++ backend/rent-service/src/main/java/com/github/manimovassagh/immo_finder/rent_service/RentServiceApplication.java
@@ -2,9 +2,6 @@ package com.github.manimovassagh.immo_finder.rent_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class RentServiceApplication {
@@ -14,11 +11,3 @@ public class RentServiceApplication {
 	}
 }
 
-@RestController
-@RequestMapping("/api/v1/rent")
-class RentController {
-	@GetMapping
-	public String getRent() {
-		return "Hello World";
-	}
-}
